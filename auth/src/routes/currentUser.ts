bf5f43b1f4ca011c93e208b45d655791686a0644
mohammad zeya ahmad <mohammadzeyaahmad@gmail.com>
2023-12-23 22:41:34 +0530

    authorization middleware added

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,22 +1,11 @@
 import express,{Request,Response} from 'express';
 import  { StatusCodes } from "http-status-codes";
-import jwt from 'jsonwebtoken';
-import {User} from "../models/user"
+import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-user';
 const router=express.Router();
-router.get('/current', (req:Request, res:Response) => {
-   if(!req.session?.jwt)
-   {
-      return  res.status(StatusCodes.METHOD_NOT_ALLOWED).send({message:"User not logged in"});
-   }
-   try
-   {
-   const payload=jwt.verify(req.session.jwt,process.env.JWT_SECRET!);
-     res.status(200).json({result:payload});
-   }
-   catch(err)
-   {
-      return  res.status(StatusCodes.UNAUTHORIZED).send({message:"Authentication failed"});
-   }
+router.get('/current',currentUser,requireAuth,(req:Request, res:Response) => {
+  
+      return  res.status(StatusCodes.OK).json({result: req.user||null});
    
 });
 
